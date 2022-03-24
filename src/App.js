6c5a913fe1407f7d1de20612de462c854131570e
cph-mtnl <mtnl@cphbusiness.dk>
2022-03-24 14:53:28 +0100

    react route practice from lecture

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import "./styles.css";
 
 function App() {
+  const buttons = ["Home", "Contact", "About"];
+  const [page, setPage] = React.useState("Home");
+
+  function handleClick(event) {
+    setPage(event.target.name);
+  }
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        {buttons.map((buttonText) => (
+          <button name={buttonText} key={buttonText} onClick={handleClick}>
+            {buttonText}
+          </button>
+        ))}
+      </nav>
+      {page == "Home" && <Home title="Home" />}
+      {page == "About" && <About title="About" />}
+      {page == "Contact" && <Contact title="Contact" />}
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home({ title }) {
+  return (
+    <div className="component red-page">
+      <h1>{title}</h1>
+    </div>
+  );
+}
